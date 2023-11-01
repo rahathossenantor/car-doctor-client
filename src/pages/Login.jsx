e1f5f55730bd1655392c3957d242e6aeb8c11801
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-11-02 02:37:27 +0600

    Register and login system has been created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
 import { FaEyeSlash, FaEye } from "react-icons/fa";
 import Swal from "sweetalert2";
+import bgImage from "../assets/images/login/login.svg";
 
 const Login = () => {
 
@@ -82,44 +83,49 @@ const Login = () => {
     };
 
     return (
-        <div className="md:container md:mx-auto 2xl:px-0 xl:px-0 lg:px-5 md:px-5 px-5 w-full flex flex-col items-center justify-center py-14">
-            <div className="text-center mb-5">
-                <h2 className="text-4xl font-medium">Login now!</h2>
-            </div>
-            <div className="bg-base-100 2xl:w-96 xl:w-96 lg:w-[380px] md:w-[380px] w-[340px] shadow-2xl rounded-xl border">
-                <form onSubmit={handleEmailPassLogin} className="p-6">
-                    <div className="form-control">
-                        <label className="label pb-1">
-                            <span className="text-lg">Email <span className="text-red-500">*</span></span>
-                        </label>
-                        <input type="email" placeholder="Email" name="email" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label pb-1">
-                            <span className="text-lg">Password <span className="text-red-500">*</span></span>
-                        </label>
-                        <div className="relative form-control">
-                            <input required type={isShow ? "text" : "password"} name="password" placeholder="Password" className="input input-bordered" />
-                            <span onClick={() => setIsShow(!isShow)} className="absolute h-full flex items-center top-0 right-3 cursor-pointer">{isShow ? <FaEyeSlash className="text-xl"></FaEyeSlash> : <FaEye className="text-xl"></FaEye>}</span>
+        <div className="md:container md:mx-auto 2xl:px-0 xl:px-0 lg:px-5 md:px-5 px-5 py-14">
+            <div className="flex flex-col lg:flex-row xl:flex-row">
+                <div className="w-full lg:w-1/2 xl:w-1/2 flex justify-center items-center">
+                    <img src={bgImage} alt="bg-image" className="inline-block" />
+                </div>
+                <div className="w-full lg:w-1/2 xl:w-1/2 flex justify-center">
+                    <form onSubmit={handleEmailPassLogin} className="p-6 w-[400px] bg-base-100 shadow-2xl">
+                        <div className="text-center mb-5">
+                            <h2 className="text-4xl font-medium">Sign In Now!</h2>
                         </div>
+                        <div className="form-control">
+                            <label className="label pb-1">
+                                <span className="text-lg">Email <span className="text-red-500">*</span></span>
+                            </label>
+                            <input type="email" placeholder="Email" name="email" className="input input-bordered rounded-none" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label pb-1">
+                                <span className="text-lg">Password <span className="text-red-500">*</span></span>
+                            </label>
+                            <div className="relative form-control">
+                                <input required type={isShow ? "text" : "password"} name="password" placeholder="Password" className="input input-bordered rounded-none" />
+                                <span onClick={() => setIsShow(!isShow)} className="absolute h-full flex items-center top-0 right-3 cursor-pointer">{isShow ? <FaEyeSlash className="text-xl"></FaEyeSlash> : <FaEye className="text-xl"></FaEye>}</span>
+                            </div>
 
-                        <div className="mt-3">
-                            <p className="">Forgot password? <a className="text-blue-700 underline font-medium cursor-pointer">Reset</a></p>
-                        </div>
+                            <div className="mt-3">
+                                <p className="">Forgot password? <a className="text-blue-700 underline font-medium cursor-pointer">Reset</a></p>
+                            </div>
 
-                    </div>
-                    <div className="form-control mt-6">
-                        <button type="submit" className="bn632-hover bn28 px-7 py-[8px] font-poppins">Login</button>
-                    </div>
-                    <p className="my-3">New to this website? <Link className="text-blue-700 underline font-medium" to="/register">Register</Link></p>
-                    <div className="text-center">
-                        <p className="text-xl mb-2">-------------or-------------</p>
-                        <div className="flex items-center justify-center gap-5">
-                            <button onClick={handleGoogleSignIn}><img src="https://i.ibb.co/7j9whwL/search.png" className="w-10 cursor-pointer" alt="Google" /></button>
-                            <button onClick={handleGitHubSignIn}><img src="https://i.ibb.co/vxj4vrN/github.png" className="w-10 cursor-pointer" alt="GtHub" /></button>
                         </div>
-                    </div>
-                </form>
+                        <div className="form-control mt-6">
+                            <button type="submit" className="border-[#FF3811] border bg-transparent text-[#FF3811] hover:bg-[#FF3811] hover:text-white px-7 py-[8px]">Login</button>
+                        </div>
+                        <p className="my-3">New to this website? <Link className="text-blue-700 underline font-medium" to="/register">Register</Link></p>
+                        <div className="text-center">
+                            <p className="text-xl mb-2">-------------or-------------</p>
+                            <div className="flex items-center justify-center gap-5">
+                                <button onClick={handleGoogleSignIn}><img src="https://i.ibb.co/7j9whwL/search.png" className="w-10 cursor-pointer" alt="Google" /></button>
+                                <button onClick={handleGitHubSignIn}><img src="https://i.ibb.co/vxj4vrN/github.png" className="w-10 cursor-pointer" alt="GtHub" /></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
             {errorStatus && <div>
                 <p className="text-red-500 text-lg font-medium">{errorStatus}</p>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,11 +4,12 @@ import { useContext, useState } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import { updateProfile } from "firebase/auth";
 import Swal from "sweetalert2";
+import bgImage from "../assets/images/login/login.svg";
 
 const Register = () => {
     const [isShow, setIsShow] = useState(false);
     const [errorStatus, setRrrorStatus] = useState("");
-    const {setProfileAvatar, registerUserWithEmailAndPass, signInUserWithGoogle, signInUserWithGitHub } = useContext(AuthContext);
+    const { setProfileAvatar, registerUserWithEmailAndPass, signInUserWithGoogle, signInUserWithGitHub } = useContext(AuthContext);
 
     const navigate = useNavigate();
 
@@ -16,10 +17,10 @@ const Register = () => {
         event.preventDefault();
 
         const name = event.target.name.value;
-        const avater = event.target.photoURL.value;
+        const avater = event.target.photoURL.value || "";
         const email = event.target.email.value;
         const pass = event.target.password.value;
-        // const terms = event.target.terms.checked;
+        const terms = event.target.terms.checked;
 
         // reset error status
         setRrrorStatus("");
@@ -36,10 +37,10 @@ const Register = () => {
             setRrrorStatus("Password must have at least one special character!");
             return;
         }
-        // else if (!terms) {
-        //     setRrrorStatus("Please accept our terms & conditions!");
-        //     return;
-        // }
+        else if (!terms) {
+            setRrrorStatus("Please accept our terms & conditions!");
+            return;
+        }
 
         // create new user account with email and password
         registerUserWithEmailAndPass(email, pass)
@@ -99,55 +100,54 @@ const Register = () => {
     };
 
     return (
-        <div className="md:container md:mx-auto 2xl:px-0 xl:px-0 lg:px-5 md:px-5 px-5 w-full flex flex-col items-center justify-center py-14">
-            <div className="text-center mb-5">
-                <h2 className="text-4xl font-medium">Register now!</h2>
-            </div>
-            <div className="bg-base-100 2xl:w-96 xl:w-96 lg:w-[380px] md:w-[380px] w-[340px] shadow-2xl rounded-xl border">
-                <form onSubmit={handleEmailPassRegister} className="p-6">
-                    <div className="form-control">
-                        <label className="label py-1">
-                            <span className="text-lg">Full name <span className="text-red-500">*</span></span>
-                        </label>
-                        <input type="text" placeholder="Full name" name="name" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label pb-1">
-                            <span className="text-lg">Photo URL</span>
-                        </label>
-                        <input type="text" name="photoURL" placeholder="Photo URL" className="input input-bordered" />
-                    </div>
-                    <div className="form-control">
-                        <label className="label pb-1">
-                            <span className="text-lg">Email <span className="text-red-500">*</span></span>
-                        </label>
-                        <input type="email" placeholder="Email" name="email" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label pb-1">
-                            <span className="text-lg">Password <span className="text-red-500">*</span></span>
-                        </label>
-                        <div className="relative form-control">
-                            <input required type={isShow ? "text" : "password"} name="password" placeholder="Password" className="input input-bordered" />
-                            <span onClick={() => setIsShow(!isShow)} className="absolute h-full flex items-center top-0 right-3 cursor-pointer">{isShow ? <FaEyeSlash className="text-xl"></FaEyeSlash> : <FaEye className="text-xl"></FaEye>}</span>
+        <div className="md:container md:mx-auto 2xl:px-0 xl:px-0 lg:px-5 md:px-5 px-5 py-14">
+            <div className="flex flex-col lg:flex-row xl:flex-row">
+                <div className="w-full lg:w-1/2 xl:w-1/2 flex justify-center items-center">
+                    <img src={bgImage} alt="bg-image" className="inline-block" />
+                </div>
+                <div className="w-full lg:w-1/2 xl:w-1/2 flex justify-center">
+                    <form onSubmit={handleEmailPassRegister} className="p-6 w-[400px] bg-base-100 shadow-2xl">
+                        <div className="text-center mb-5">
+                            <h2 className="text-4xl font-medium">Sign Up Now!</h2>
                         </div>
-                        <div className="mt-3">
-                            <input type="checkbox" className="cursor-pointer" name="terms" id="terms" />
-                            <label className="ml-2 cursor-pointer" htmlFor="terms">Accept our terms & conditions</label>
+                        <div className="form-control">
+                            <label className="label py-1">
+                                <span className="text-lg">Full name <span className="text-red-500">*</span></span>
+                            </label>
+                            <input type="text" placeholder="Full name" name="name" className="input input-bordered rounded-none" required />
                         </div>
-                    </div>
-                    <div className="form-control mt-6">
-                        <button type="submit" className="bn632-hover bn28 px-7 py-[8px] font-poppins">Register</button>
-                    </div>
-                    <p className="my-3">Already have an acount? <Link className="text-blue-700 underline font-medium" to="/login">Login</Link></p>
-                    <div className="text-center">
-                        <p className="text-xl mb-2">-------------or-------------</p>
-                        <div className="flex items-center justify-center gap-5">
-                            <button onClick={handleGoogleRegister}><img src="https://i.ibb.co/7j9whwL/search.png" className="w-10 cursor-pointer" alt="Google" /></button>
-                            <button onClick={handleGitHubRegister}><img src="https://i.ibb.co/vxj4vrN/github.png" className="w-10 cursor-pointer" alt="GtHub" /></button>
+                        <div className="form-control">
+                            <label className="label pb-1">
+                                <span className="text-lg">Email <span className="text-red-500">*</span></span>
+                            </label>
+                            <input type="email" placeholder="Email" name="email" className="input input-bordered rounded-none" required />
                         </div>
-                    </div>
-                </form>
+                        <div className="form-control">
+                            <label className="label pb-1">
+                                <span className="text-lg">Password <span className="text-red-500">*</span></span>
+                            </label>
+                            <div className="relative form-control">
+                                <input required type={isShow ? "text" : "password"} name="password" placeholder="Password" className="input input-bordered rounded-none" />
+                                <span onClick={() => setIsShow(!isShow)} className="absolute h-full flex items-center top-0 right-3 cursor-pointer">{isShow ? <FaEyeSlash className="text-xl"></FaEyeSlash> : <FaEye className="text-xl"></FaEye>}</span>
+                            </div>
+                            <div className="mt-3">
+                                <input type="checkbox" className="cursor-pointer" name="terms" id="terms" />
+                                <label className="ml-2 cursor-pointer" htmlFor="terms">Accept our terms & conditions</label>
+                            </div>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button type="submit" className="px-7 py-[8px] border-[#FF3811] border bg-transparent text-[#FF3811] hover:bg-[#FF3811] hover:text-white">Register</button>
+                        </div>
+                        <p className="my-3">Already have an acount? <Link className="text-blue-700 underline font-medium" to="/login">Login</Link></p>
+                        <div className="text-center">
+                            <p className="text-xl mb-2">-------------or-------------</p>
+                            <div className="flex items-center justify-center gap-5">
+                                <button onClick={handleGoogleRegister}><img src="https://i.ibb.co/7j9whwL/search.png" className="w-10 cursor-pointer" alt="Google" /></button>
+                                <button onClick={handleGitHubRegister}><img src="https://i.ibb.co/vxj4vrN/github.png" className="w-10 cursor-pointer" alt="GtHub" /></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
             {errorStatus && <div>
                 <p className="text-red-500 text-lg font-medium">{errorStatus}</p>
